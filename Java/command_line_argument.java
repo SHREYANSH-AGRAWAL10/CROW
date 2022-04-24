f41d3d476f63058c8a63144dbefd831b58a0607e
CROW <102203352+SHREYANSH-AGRAWAL10@users.noreply.github.com>
2022-04-24 16:05:06 +0530

    Create command_line_argument.java

---
+++ Java/command_line_argument.java
@@ -0,0 +1,13 @@
+//taking input as command line argument
+
+package com.company;
+
+public class Q7 {
+    public static void main(String[] args){
+        String name,course,roll,sem;
+        name = args[0];
+        course = args[1];
+        roll = args[2];
+        sem = args[3];
+    }
+}
