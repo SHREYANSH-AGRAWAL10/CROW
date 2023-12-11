a4201f4bd0dd65963f4e430ef2b2de42472d53c5
CROW <102203352+SHREYANSH-AGRAWAL10@users.noreply.github.com>
2023-12-11 17:00:48 +0530

    Add files via upload

---
+++ res/l2.php
@@ -0,0 +1,16 @@
+<?php
+
+
+if(!empty($_POST["remember"])) {
+	setcookie ("username",$_POST["username"],time()+ 3600);
+	setcookie ("password",$_POST["password"],time()+ 3600);
+	echo "Cookies Set Successfuly";
+} else {
+	setcookie("username","");
+	setcookie("password","");
+	echo "Cookies Not Set";
+}
+
+?>
+
+<p><a href="l.php"> Go to Login Page </a> </p>
